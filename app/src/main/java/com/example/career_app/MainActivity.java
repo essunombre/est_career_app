118b35ae740f1109f0165f3bac1777df581bd77f
Oswaldo Rodriguez <Owner2303@gmail.com>
2021-11-09 10:31:24 -0500

    login working and grabbing inputs

--- app/src/main/java/com/example/career_app/MainActivity.java
+++ app/src/main/java/com/example/career_app/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.career_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.email) TextView email;
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.password) TextView password;
+
+    @OnClick(R.id.login) void loginButton(View view ){
+        Toast.makeText(this,email.getText() + " " + password.getText(), Toast.LENGTH_SHORT).show();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
