7bf48a57e1f2e40d7f1a712c38b7d15c6b152220
Oswaldo Rodriguez <Owner2303@gmail.com>
2021-11-09 14:11:56 -0500

    adding nav bar

--- app/src/main/java/com/example/career_app/MainActivity.java
+++ app/src/main/java/com/example/career_app/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.career_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,16 +17,30 @@ import butterknife.OnClick;
 public class MainActivity extends AppCompatActivity {
     @SuppressLint("NonConstantResourceId")
     @BindView(R.id.email) TextView email;
+
     @SuppressLint("NonConstantResourceId")
     @BindView(R.id.password) TextView password;
 
-    @OnClick(R.id.login) void loginButton(View view ){
-        Toast.makeText(this,email.getText() + " " + password.getText(), Toast.LENGTH_SHORT).show();
-    }
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.create_account) TextView createAccount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
     }
+
+    @SuppressLint("NonConstantResourceId")
+    @OnClick(R.id.login) void loginButton(View view ){
+        Toast.makeText(this,email.getText() + " " + password.getText(), Toast.LENGTH_SHORT).show();
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @OnClick(R.id.create_account) void createAccount(View view ){
+        Toast.makeText(this,email.getText() + " " + password.getText(), Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, Dashboard.class);
+        startActivity(intent);
+    }
+
 }
