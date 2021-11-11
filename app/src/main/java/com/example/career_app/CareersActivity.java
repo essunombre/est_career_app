f191b8d07a0080c69af7b1e1211c1bffcd93c60c
Oswaldo Rodriguez <Owner2303@gmail.com>
2021-11-11 13:12:17 -0500

    all views connected

--- app/src/main/java/com/example/career_app/CareersActivity.java
+++ app/src/main/java/com/example/career_app/CareersActivity.java
@@ -6,38 +6,31 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class UpdateProfile extends AppCompatActivity {
-
-
-    @SuppressLint("NonConstantResourceId")
-    @BindView(R.id.hiAngie) TextView angie;
+public class CareersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_update_profile);
+        setContentView(R.layout.activity_careers);
         ButterKnife.bind(this);
     }
-
 
     //Account Button
     @SuppressLint("NonConstantResourceId")
     @OnClick(R.id.accountButton) void clickAccount(View view){
-        Toast.makeText(this, "You are in the Account Page Already!", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Welcome to the accounts!", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, UpdateProfile.class);
+        startActivity(intent);
     }
     //Search Careers Button
     @SuppressLint("NonConstantResourceId")
     @OnClick(R.id.careersSearch) void careersSearch(View view){
-        Toast.makeText(this, "Welcome to the Careers Page!", Toast.LENGTH_LONG).show();
-        Intent intent = new Intent(this, CareersActivity.class);
-        startActivity(intent);
+        Toast.makeText(this, "You are in the Careers Page already!", Toast.LENGTH_LONG).show();
     }
     //Home Button
     @SuppressLint("NonConstantResourceId")
