c2addf99f31cd887727ed46bee1a515d22ad751f
Oswaldo Rodriguez <Owner2303@gmail.com>
2021-11-10 23:00:36 -0500

    sign in button working

--- app/src/main/java/com/example/career_app/MainActivity.java
+++ app/src/main/java/com/example/career_app/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.career_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
@@ -14,20 +20,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView register;
     private TextView forgotPassword;
 
+    private FirebaseAuth mAuth;
+    private static final String TAG = "MainActivity";
+    private EditText editTextEmail, editTextPassword;
+
+    //Whatever Sign in button does, goes inside this method.
+    @SuppressLint("NonConstantResourceId")
     @OnClick(R.id.signIn) void signIn(View view){
         Intent intent = new Intent(this, Home.class);
         startActivity(intent);
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
         register = (TextView) findViewById(R.id.register);
         register.setOnClickListener(this);
 
+        editTextEmail = (EditText) findViewById(R.id.email);
+        editTextPassword = (EditText) findViewById(R.id.password);
+
         forgotPassword = (TextView) findViewById((R.id.forgotPassword));
         forgotPassword.setOnClickListener(this);
-        ButterKnife.bind(this);
+
     }
 
     @Override
